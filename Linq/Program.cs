d64d833af3c3a5fce624442e14e6add7e6c31286
Joakim Olofsson <joakim.olofsson@iths.se>
2024-10-10 15:50:23 +0200

    IEnumerable exercise

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var numbers = new string[]{ "1", "2", "3", "4", "5" };
+            var numbers2 = new List<int>() { 6, 7, 8, 9, 10 };
+            DisplayList(numbers);
+            DisplayList(numbers2);
+        }
+
+        public static void DisplayList<T>(IEnumerable<T> numbers)
+        {
+            foreach(var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
