c764d96f9d22c73794fb4976e1989ebc01dc2606
Joakim Olofsson <joakim.olofsson@iths.se>
2024-10-11 13:15:13 +0200

    More exercises done

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -4,12 +4,23 @@
     {
         static void Main(string[] args)
         {
-            var numbers = new string[]{ "1", "2", "3", "4", "5" };
+            var numbers = new string[] { "1", "2", "3", "4", "5" };
             var numbers2 = new List<int>() { 6, 7, 8, 9, 10 };
             DisplayList(numbers);
             DisplayList(numbers2);
-        }
 
+            Movie myMovie = new Movie { Name = "Movie", Year = 1986 };
+            Book myBook = new Book { Name = "Book", Year = 1996 };
+
+            myMovie.Show();
+            myBook.Show();
+
+            var myClasses = new List<IMedia> { 
+                new Movie { Name = "Movie", Year = 1986 },
+                new Book { Name = "Book", Year = 1996 }
+            };
+            myClasses.ShowList();
+        }
         public static void DisplayList<T>(IEnumerable<T> numbers)
         {
             foreach(var item in numbers)
@@ -18,4 +29,39 @@
             }
         }
     }
+
+    public interface IMedia
+    {
+        string Name { get; set; }
+        int Year { get; set; }
+    }
+
+    class Movie : IMedia
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+
+    class Book : IMedia
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+
+    public static class MediaExtensions
+    {
+        public static void Show(this IMedia media)
+        {
+            Console.WriteLine($"{media.Name} | {media.Year}");
+        }
+
+        public static void ShowList(this IEnumerable<IMedia> media)
+        {
+            foreach(IMedia item in media)
+            {
+                Console.WriteLine(item.Name + " " + item.Year);
+            }
+        }
+    }
 }
+
