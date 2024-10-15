3c8b2a290c80ee224838566d237386acd393cd70
Joakim Olofsson <joakim.olofsson@iths.se>
2024-10-15 15:43:28 +0200

    IEnumerable, Func and Linq done

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -20,12 +20,73 @@
                 new Book { Name = "Book", Year = 1996 }
             };
             myClasses.ShowList();
+
+            Func<int, int> multiply = (x) => x * 2;
+            Console.WriteLine(multiply(5));
+
+            Func<int, int, int> add = (x, y) => x + y;
+            Console.WriteLine(add(5,5));
+
+            Func<string, string> versal = (x) => x.ToUpper();
+            Console.WriteLine(versal("Bacon"));
+
+            ProcessNumber(add, 100);
+            Console.WriteLine(GenericProcess(20, multiply));
+            Console.WriteLine(GenericProcess("Apple", versal));
+
+            List<int> myNumbers = Enumerable.Range(1, 10).ToList();
+            var evenNumbers = myNumbers.Where(n => n % 2 == 0);
+            Looper(evenNumbers);
+
+            List<int> myInts = Enumerable.Range(1, 5).ToList();
+            var intsToDouble = myInts.Select(n => n / 0.33);
+            Looper(intsToDouble);
+
+            var myNames = new List<string> { "Alice", "Charlie", "Bob", "Eve", "David" };
+            var mySortedNames = myNames.OrderBy(n => n);
+            Looper(mySortedNames);
+
+            var randomNumbers = new List<int> { 10, 3, 5, 2, 8, 7 };
+            var manipulateNumbers = randomNumbers.Where(num => num > 5)
+                .Select(num => num * 0.33)
+                .OrderByDescending(num => num);
+            Looper(manipulateNumbers);
         }
-        public static void DisplayList<T>(IEnumerable<T> numbers)
+
+        public static void Looper<T>(IEnumerable<T> items)
         {
-            foreach(var item in numbers)
+            foreach (var item in items)
             {
                 Console.WriteLine(item);
+            }
+        }
+
+        public static TResult GenericProcess<T, TResult>(T input, Func<T, TResult> operation)
+        {
+            return operation(input);
+        }
+
+        public static void ProcessNumber(Func<int, int, int> operation, int y)
+        {
+            int num = 10;
+            int sum = operation(y, num);
+            Console.WriteLine(sum);
+        }
+
+        public static void DisplayList<T>(IEnumerable<T> numbers)
+        {
+            /*foreach(var item in numbers)
+            {
+                Console.WriteLine(item);
+            }*/
+
+            using(IEnumerator<T> enumerator = numbers.GetEnumerator())
+            {
+                while(enumerator.MoveNext())
+                {
+                    T item = enumerator.Current;
+                    Console.WriteLine(item);
+                }
             }
         }
     }
